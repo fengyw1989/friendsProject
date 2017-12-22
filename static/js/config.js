7594aa2aa08859567891d17aea2858138e9d7daa
fengyw1989 <793268593@qq.com>
2017-12-22 17:40:00 +0800

    first commit

---
+++ static/js/config.js
@@ -0,0 +1,5 @@
+/**
+ * Created by 79326 on 2017/12/19.
+ */
+window.newBaseUrl = 'http://192.168.20.210:8069/'
+window.newBaseurlMock = 'http://result.eolinker.com/CRnBNlnd90d21b4ed30412a935a6c1c42aa076d50dc0c66?uri='
