3e941ff2f67f36f3fc8eed004ad64227029cb7b2
fengyw1989 <793268593@qq.com>
2018-02-27 16:28:28 +0800

    first

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 /**
  * Created by 79326 on 2017/12/19.
  */
-window.newBaseUrl = 'http://192.168.20.210:8069/'
-window.newBaseurlMock = 'http://result.eolinker.com/CRnBNlnd90d21b4ed30412a935a6c1c42aa076d50dc0c66?uri='
+window.newBaseUrl = 'https://192.168.20.190:8069/'
+window.newBaseurlMock = 'https://result.eolinker.com/CRnBNlnd90d21b4ed30412a935a6c1c42aa076d50dc0c66?uri='
